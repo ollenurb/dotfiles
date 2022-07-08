12e1504047720663cac0f22454b64c6974f1b0b4
Ollenurb <brunello.mtt@gmail.com>
2022-07-08 14:10:02 +0200

    Picom: Added blur, modified topbar transparency

--- programs/picom/default.nix
+++ programs/picom/default.nix
@@ -3,22 +3,24 @@
 {
   services.picom = {
     enable = true;
+    experimentalBackends = true;
     vSync = true;
     shadow = true;
     shadowOpacity = "0.6";
     shadowOffsets = [ 0  (-2) ];
     fade = true;
-    fadeDelta = 5;
-    fadeSteps = [ "0.03"  "0.03" ];
+    fadeSteps = [ "0.04"  "0.04" ];
     extraOptions = ''
       xrender-sync-fence = true;
+      blur-method = "dual_kawase"
+      blur-strenght = 8;
     '';
     blur = true;
     opacityRule = [
       "85:class_g = 'URxvt'"
       "85:name = 'Alacritty' && focused"
       "85:class_g = 'Alacritty' && !focused"
-      "60:class_g = 'Polybar'"
+      "85:class_g = 'Polybar'"
     ];
   };
 }
